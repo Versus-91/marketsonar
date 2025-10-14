025e431eba0cb74237a20eec56de660b33d0e54b
Ghader <gharibyan.ghader@gmail.com>
2025-10-14 23:31:34 +0200

    adding db context

---
+++ Data/MarketSonarDbContext.cs
@@ -0,0 +1,9 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketSonar.Data;
+
+public class MarketSonarDbContext(DbContextOptions<MarketSonarDbContext> options) : DbContext(options)
+{
+
+}
