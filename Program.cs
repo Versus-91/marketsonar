aa960dd409b4b38b484fe002e9567f658b9eb1a2
Ghader <gharibyan.ghader@gmail.com>
2025-10-26 21:11:33 +0100

    updating app settings and integrating the app with azure key vault

--- Program.cs
+++ Program.cs
@@ -1,25 +1,37 @@
 using MarketSonar.Data;
-
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Azure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MarketSonarDbContext>();
+builder.Services.AddAzureClients(clientBuilder => {
+    clientBuilder.AddSecretClient(new Uri(builder.Configuration["KeyVault:Endpoint"]));
+    clientBuilder.UseCredential(new  DefaultAzureCredential());
+});
+builder.Services.AddMvc(options =>options.EnableEndpointRouting = false);
 var app = builder.Build();
 
 app.UseStaticFiles();
-app.MapFallbackToFile("index.html");
-// Configure the HTTP request pipeline.
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseMvc();
+// app.MapControllerRoute(name:"default",
+//     pattern:"{controller= Home}/{action=Index}/{id?}");
+// app.MapFallbackToFile("index.html")
+// ;
 app.Run();
