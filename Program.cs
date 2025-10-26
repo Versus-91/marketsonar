17be503bc06d1cf68107f62212cac00e5777da6f
Ghader <gharibyan.ghader@gmail.com>
2025-10-26 18:25:34 +0100

    serve static index html file

--- Program.cs
+++ Program.cs
@@ -6,19 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MarketSonarDbContext>();
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.MapFallbackToFile("index.html");
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.MapGet("/", () => Results.Ok("Azar Khare Gave Mane") );
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
