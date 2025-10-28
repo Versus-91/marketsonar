f48bf2b8ff6ce82d2b813e97c760cf827eb9bb48
Ghader <gharibyan.ghader@gmail.com>
2025-10-29 00:57:51 +0100

    adding home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Azure;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketSonar.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class HomeController(SecretClient secretClient,BlobServiceClient storageService,ILogger<HomeController> logger): ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Upload(IFormFile file,CancellationToken cancellationToken)
+    {
+        await using Stream stream = file.OpenReadStream();
+        var blobContainerClient =  storageService.GetBlobContainerClient("images");
+        await blobContainerClient.CreateIfNotExistsAsync(publicAccessType:PublicAccessType.Blob,cancellationToken: cancellationToken);
+        try
+        {
+            var storageResult = await blobContainerClient.UploadBlobAsync(file.FileName, stream, cancellationToken);
+            return Ok(storageResult);
+        }
+        catch (RequestFailedException e)
+        {
+            logger.LogError(e.ToString());
+            return BadRequest(e.Message);
+        }
+        
+    }
+}
