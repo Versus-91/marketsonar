8fcf3cd0c05245e5775dbaa0d610473cd0013985
Ghader <gharibyan.ghader@gmail.com>
2025-11-01 00:55:53 +0100

    fixing typo

---
+++ extensions/BlobStorageExtension.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs;
+
+namespace MarketSonar.extensions;
+
+public static class BlobStorageExtension
+{
+    public static void AddBlobStorage(this IServiceCollection services) {
+        services.AddSingleton<BlobServiceClient>(provider =>
+        {
+            var serviceClient = provider.GetService<SecretClient>();
+            var storageConnectionString = serviceClient?.GetSecret("StorageDeckConnectionString");
+            return new BlobServiceClient(
+                storageConnectionString?.Value.Value
+            );
+        });
+    }
+}
