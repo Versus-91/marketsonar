32bdf315724d91add3f71d0936e527877f59ece1
Ghader <gharibyan.ghader@gmail.com>
2025-11-01 16:10:21 +0100

    adding contact us view

---
+++ frontend/market-sonar/app/pages/contact.vue
@@ -0,0 +1,39 @@
+﻿<script setup lang="ts">
+import {BField, BInput} from "buefy";
+
+definePageMeta({
+  layout:'fullpage'
+})
+</script>
+
+<template>
+  <div class="container">
+    <div class="columns is-centered">
+      <div class="column is-6">
+        <div class="section">
+          <h1 class="title is-4">Contact Us</h1>
+          <hr>
+          <b-field label="Name" >
+            <b-input name="name" type="text"></b-input>
+          </b-field>
+          <b-field label="Email" >
+            <b-input name="email" type="email"></b-input>
+          </b-field>
+          <b-field label="Message" >
+            <b-input name="message" type="textarea"></b-input>
+          </b-field>
+          <button class="button mx-1">Send</button>
+          <a class="button is-link" href="/">Back</a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
